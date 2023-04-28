ae45d7d9c38a50a514ea8181c7c1205f16a7f3b7
pratikshaGorgal <119149138+pratikshaGorgal@users.noreply.github.com>
2023-04-28 22:35:08 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CustomStyleDirective } from './custom-style.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomStyleDirective,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/custom-style.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomStyle]'
+})
+export class CustomStyleDirective {
+
+  constructor(public obj:ElementRef) {
+    obj.nativeElement.style.background='Yellow';
+  }
+
+}
